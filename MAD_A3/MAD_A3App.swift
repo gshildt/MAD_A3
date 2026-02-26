2281d68fd4e897f567662383c94a445acfc8026e
Greg Shildt <gshildt@okstate.edu>
2026-02-26 12:56:00 -0600

    Initial Commit

---
+++ MAD_A3/MAD_A3App.swift
@@ -0,0 +1,17 @@
+//
+//  MAD_A3App.swift
+//  MAD_A3
+//
+//  Created by Greg Shildt on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct MAD_A3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
